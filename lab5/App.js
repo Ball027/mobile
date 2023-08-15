ea713f30f61b47e1c55fffa93e13014f9b2a0d05
Natchanon Ruengsrinaraporn <88502240+Ball027@users.noreply.github.com>
2023-08-15 16:27:49 +0700

    Add files via upload

---
+++ lab5/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import CategoriesScreen from "./screens/CategoriesScreen";
+import CategoryMealsScreen from "./screens/CategoryMealsScreen";
+import MealDetailScreen from "./screens/MealDetailScreen";
+// import คอมโพเนนต์ที่จำเป็น
+
+const MealsNavigator = createNativeStackNavigator();
+
+export default function App() {
+  // เพิ่มโค้ดส่วนนี้ เพื่อจัดการ Stack Navigation
+  return(
+  <NavigationContainer>
+    <MealsNavigator.Navigator initialRouteName="Categories" screenOptions={{ headerStyle: { backgroundColor: "#4a148c", },headerTintColor:"white", }}>
+      <MealsNavigator.Screen name="Categories" component={CategoriesScreen}
+       options={{title: "Meal Categories"
+                }} />
+      <MealsNavigator.Screen name="CategoryMeals" component={CategoryMealsScreen}/>
+      <MealsNavigator.Screen name="MealDetail" component={MealDetailScreen}/>
+    </MealsNavigator.Navigator>
+  </NavigationContainer>)
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "blue",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
